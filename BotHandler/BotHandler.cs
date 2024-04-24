12631a64534ecadfc1817fc4676ef1b527d06a67
QForm <mr.bogdan2003@gmail.com>
2024-04-25 00:56:13 +0600

    style: delete useless code

--- BotHandler/BotHandler.cs
+++ BotHandler/BotHandler.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Threading;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using TokenHandler;
-using WeatherHandler;
 
 namespace BotHandler;
 
