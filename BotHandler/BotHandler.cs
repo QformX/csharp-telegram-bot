56dc9a78e8b8c32d8c9464ea88a58f78bd8c6e9e
QForm <mr.bogdan2003@gmail.com>
2024-04-24 14:04:28 +0600

    feat: add base update handler

--- BotHandler/BotHandler.cs
+++ BotHandler/BotHandler.cs
@@ -1,19 +1,72 @@
-﻿using Telegram.Bot;
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TokenHandler;
+using WeatherHandler;
 
 namespace BotHandler;
 
 public class BotHandler
 {
-    public async static void Start()
+    public BotHandler() { }
+    public async void Start()
     {
         string path = "..\\..\\..\\..\\BotHandler\\encrypted.bin";
         var token = new Tokenizer(path).Token();
         var botClient = new TelegramBotClient(token);
-        var me = await botClient.GetMeAsync();
-        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
-        var handler = new WeatherHandler.WeatherHandler("Москва");
+        using CancellationTokenSource cts = new();
+
+        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+    }
+
+    async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Only process Message updates: https://core.telegram.org/bots/api#message
+        if (update.Message is not { } message)
+            return;
+        // Only process text messages
+        if (message.Text is not { } messageText)
+            return;
+
+        var chatId = message.Chat.Id;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+        var cur_city = "омск";
+        var handler = new WeatherHandler.WeatherHandler($"{cur_city}");
         var forecast = handler.GetForecast();
-        Console.WriteLine(forecast.BuildMessage());
+        var forecastmessage = forecast.BuildMessage();
+
+        // Echo received message text
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: forecastmessage,
+            cancellationToken: cancellationToken);
+    }
+
+    Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
     }
 }
