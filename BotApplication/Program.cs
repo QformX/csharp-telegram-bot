91ddb2f17c8da41c404f1e649a7e116229fc4ea2
QForm <mr.bogdan2003@gmail.com>
2024-04-25 00:52:17 +0600

    feat: add basic logic with SQL

--- BotApplication/Program.cs
+++ BotApplication/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace BotApplication
+﻿using SQLDatabase;
+using Telegram.Bot.Types;
+
+namespace BotApplication
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var sql = new SQLDatabase.SQLHandler();
+            sql.IniInitialize();
             var bot = new BotHandler.BotHandler();
             bot.Start();
             Console.ReadKey();
