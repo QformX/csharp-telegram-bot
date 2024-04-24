e0ad5e492d7ae830e4b6febeb6f7d2b5fb540366
QForm <mr.bogdan2003@gmail.com>
2024-04-24 21:31:59 +0600

    feat: fix bug in message handler

---
+++ BotHandler/UserHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace BotHandler
+{
+    internal class UserHandler
+    {
+        private Dictionary<long, bool> _usersCallback;
+
+        public UserHandler() 
+        {
+            _usersCallback = new Dictionary<long, bool>();
+        }
+
+        public void Callback(long userId)
+        {
+
+            _usersCallback[userId] = !_usersCallback[userId];
+        }
+
+        public void Add(long userId)
+        {
+
+            _usersCallback.Add(userId, false);
+        }
+
+        public bool UserExist(long userId)
+        {
+            return _usersCallback.ContainsKey(userId);
+        }
+
+        public bool IsUserCallback(long userId)
+        {
+            return _usersCallback[userId];
+        }
+    }
+}
